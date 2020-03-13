4d56a44f098ca120f70c1936ed4d8466495b39b7
jyyoo1 <jyyoo1@pentasecurity.com>
2020-03-13 11:21:30 +0900

    페이지 준비중

--- vue/src/views/Storages.vue
+++ vue/src/views/Storages.vue
@@ -1,9 +1,14 @@
 <template>
-  <v-layout justify-space-around row fill-height mt-12>
-    <v-flex xs12 class="text-center">
-      <span class="text-center display-3">storages</span>
-    </v-flex>
-  </v-layout>
+  <div id="page-storages">
+    <v-container  mt-12>
+      <v-row class="text-center" align="center" justify="center">
+        <v-icon size="100" color="coinYellow">info</v-icon>
+      </v-row>
+      <v-row class="text-center mt-6" align="center" justify="center">
+        <div class="text-center display-1">페이지 <span class="font-weight-bold">준비중</span>입니다.</div>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
