c63d333920a02d256d5a4ff5d813121fac51e154
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-03-16 14:12:25 +0900

    Setup CORS

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 //var networksRouter = require('./routes/networks');
@@ -14,6 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors({origin: 'http://localhost:8080', optionsSuccessStatus: 200}));
 
 app.param('chain_id', function(req, res, next, val) {
   console.log('chain_id');
--- vue/src/api/index.js
+++ vue/src/api/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
 // axios.defaults.withCredentials = true;
+const server = 'http://localhost:3000';
+const options = { headers: { 
+  // for CORS
+  'Access-Control-Allow-Origin': '*',
+} };
 
 export default {
 
