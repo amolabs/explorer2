ab675b45db141f35f8f067af03e6c36c48df2aa8
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-03-17 16:26:05 +0900

    [vue] clean up

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -3,15 +3,4 @@ module.exports = {
     "vuetify"
   ],
   "lintOnSave": false,
-  devServer: {
-    proxy: {
-      '/api' : { // /api로 오는 모든 요청 Backend 서버로 패스
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        // pathRewrite: {
-        //   '^/api' : ''  // /api/test2로 요청 시 -> http://localshot:3000/test2 로 변환
-        // }
-      }
-    }
-  }
 }
