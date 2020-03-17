eee203034b7f531fce4353c720108dc6812c3197
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-03-17 16:24:52 +0900

    [vue] improve $byteCalc

--- vue/src/plugin.js
+++ vue/src/plugin.js
@@ -1,34 +1,22 @@
 const globalMethods = {};
 
 globalMethods.install = function (Vue, options) {
-
-  // 전역 method
   Vue.prototype.$byteCalc = (param) => {
-
-    let data = parseInt(param);
-    let length = data.toString().length;
-
-    // TB : 1000000000000
-    // GB : 1000000000
-    let res = data;
-    let byte = '';
-
-    if (length > 12) {
-      res = data / 1000000000000;
-      byte = 'T'
-    } else if (length > 9) {
-      res = data / 1000000000;
-      byte = 'G'
-    } else if (length > 6) {
-      res = data / 1000000;
-      byte = 'M';
-    } else if (length > 3) {
-      res = data / 1000;
-      byte = 'K';
+    //console.log('byte calc', param);
+    let data;
+    if (param === undefined) {
+      data = 0;
     } else {
-      res = param
+      data = parseInt(param);
     }
-    return Number(res.toFixed(2)).toLocaleString() + ' ' +  byte;
+    let units = ['', 'K', 'M', 'G', 'T'];
+
+    var idx = 0;
+    while (data >= 1024 && idx < 4) {
+      idx++;
+      data /= 1024;
+    }
+    return Number(data.toFixed(2)).toLocaleString() + ' ' +  units[idx];
   };
 };
 
