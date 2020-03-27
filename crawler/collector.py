36cdeaacf35ff1000c59d625d7e3963793a65991
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-03-28 01:34:46 +0900

    [crawler] use session based http requests

--- crawler/collector.py
+++ crawler/collector.py
@@ -12,18 +12,18 @@ def block(s, node, height):
     txs_results = json.loads(r.text)['result']['txs_results']
     return block_id, block, txs_results
 
-def block_metas(node, base, num):
-    print(f'batch height from {base+1} to {base+num}')
-    r = requests.get(
-            f'{node}/blockchain?minHeight={base+1}&maxHeight={base+num}')
-    metas = json.loads(r.text)['result']['block_metas']
-    list.sort(metas, key=lambda val: int(val['header']['height']))
-    return metas
+#def block_metas(node, base, num):
+#    print(f'batch height from {base+1} to {base+num}')
+#    r = requests.get(
+#            f'{node}/blockchain?minHeight={base+1}&maxHeight={base+num}')
+#    metas = json.loads(r.text)['result']['block_metas']
+#    list.sort(metas, key=lambda val: int(val['header']['height']))
+#    return metas
 
-def block_txs(node, height):
-    # collect txs
-    r = requests.get(f'{node}/tx_search?query="tx.height={height}"')
-    items = json.loads(r.text)['result']['txs']
-    print(f'- block height {height}: num_txs = {len(items)}')
-    return items
+#def block_txs(node, height):
+#    # collect txs
+#    r = requests.get(f'{node}/tx_search?query="tx.height={height}"')
+#    items = json.loads(r.text)['result']['txs']
+#    print(f'- block height {height}: num_txs = {len(items)}')
+#    return items
 
