b171da625ea1991be7663fb347923e630e6676d2
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-03-27 18:34:17 +0900

    [crawler] stake tx

--- crawler/state.py
+++ crawler/state.py
@@ -7,6 +7,8 @@ class Account:
     def __init__(self, chain_id, address, cursor):
         self.chain_id = chain_id
         self.address = address
+        self.balance = 0
+        self.stake = 0
         cursor.execute("""
             SELECT * FROM `accounts`
             WHERE (`chain_id` = %(chain_id)s AND `address` = %(address)s)
@@ -16,8 +18,8 @@ class Account:
         if row:
             d = dict(zip(cursor.column_names, row))
             self.balance = int(d['balance'])
+            self.stake = int(d['stake'])
         else:
-            self.balance = 0
             cursor.execute("""
                 INSERT INTO `accounts` (`chain_id`, `address`)
                 VALUES (%(chain_id)s, %(address)s)
@@ -30,7 +32,9 @@ class Account:
         values['balance'] = str(values['balance'])
         cursor.execute("""
             UPDATE `accounts`
-            SET `balance` = %(balance)s
+            SET
+                `balance` = %(balance)s,
+                `stake` = %(stake)s
             WHERE (`chain_id` = %(chain_id)s AND `address` = %(address)s)
             """,
             values)
@@ -45,11 +49,25 @@ def transfer(tx, cursor):
     payload['amount'] = int(payload['amount'])
     sender = Account(tx.chain_id, tx.sender, cursor)
     recp = Account(tx.chain_id, payload['to'], cursor)
+
     sender.balance -= payload['amount']
     recp.balance += payload['amount']
+
     sender.save(cursor)
     recp.save(cursor)
 
+def stake(tx, cursor):
+    print(f'tx type ({tx.type})')
+    payload = json.loads(tx.payload)
+    payload['amount'] = int(payload['amount'])
+    sender = Account(tx.chain_id, tx.sender, cursor)
+
+    sender.stake += payload['amount']
+    sender.balance -= payload['amount']
+
+    sender.save(cursor)
+
 processor = {
         'transfer': transfer,
+        'stake': stake,
         }
