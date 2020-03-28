c52bbb6553d9056071801db2c59c19463372a299
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-03-29 00:55:04 +0900

    [crawler] collector class

--- crawler/collector.py
+++ crawler/collector.py
@@ -3,8 +3,150 @@
 
 import json
 import base64
+import requests
 
-def block(s, node, height):
+import amo
+
+class Collector:
+    def __init__(self, node, db):
+        self.node = node
+        self.db = db
+
+        cur = self.db.cursor()
+        # get node status
+        r = requests.get(f'{self.node}/status')
+        dat = json.loads(r.text)
+        self.remote_height = int(dat['result']['sync_info']['latest_block_height'])
+        self.chain_id = dat['result']['node_info']['network']
+
+        # get current explorer state
+        cur.execute("""SELECT `height` FROM `c_blocks` 
+            WHERE (`chain_id` = %(chain_id)s)
+            ORDER BY `height` DESC LIMIT 1""",
+            self._vars())
+        row = cur.fetchone()
+        if row:
+            #print(row)
+            b = dict(zip(cur.column_names, row))
+            self.height = int(b['height'])
+        else:
+            self.height = 0
+
+        cur.close()
+
+    def _vars(self):
+        v = vars(self).copy()
+        del v['db']
+        return v
+
+    def stat(self):
+        print(f'[crawler] node: {self.node}, local {self.height} => remote {self.remote_height}')
+
+    def genesis(self, s):
+        cur = self.db.cursor()
+        # check genesis
+        cur.execute("""
+            SELECT COUNT(*) FROM `c_genesis` WHERE (`chain_id` = %(chain_id)s)
+            """,
+            self._vars())
+        row = cur.fetchone()
+        if row[0] == 0:
+            r = s.get(f'{self.node}/genesis')
+            dat = json.loads(r.text)
+            self.genesis = json.dumps(dat['result']['genesis'])
+            cur.execute("""
+                INSERT INTO `c_genesis`
+                    (`chain_id`, `genesis`)
+                VALUES
+                    (%(chain_id)s, %(genesis)s)
+                """,
+                self._vars())
+            self.db.commit()
+        cur.close()
+
+    def play(self, limit):
+        cur = self.db.cursor()
+        s = requests.Session()
+
+        self.genesis(s)
+
+        # figure out
+        if limit > 0:
+            run = min(self.remote_height - self.height, limit)
+        else:
+            run = self.remote_height - self.height
+
+        print(f'==========================================================')
+        batch_base = self.height
+        for h in range(self.height + 1, self.height + run + 1):
+            # batch start
+            self.db.autocommit = False
+
+            ### NOTE: the following strategy is effective when tx density is high.
+            if h % 50 == 0:
+                print('.', flush=True)
+            else:
+                print('.', end='', flush=True)
+            if h % 100 == 0:
+                print(f'block height {h}', flush=True)
+            block_id, block_raw, txs_results, incs = collect_block(s, self.node, h)
+            block_header = block_raw['header']
+            tx_bodies = block_raw['data']['txs']
+            block = amo.Block(block_header['chain_id'], block_header['height'])
+            block.set_meta(block_id, block_header)
+            block.incentives = incs
+            block.save(cur)
+            num = 0
+            num_valid = 0
+            num_invalid = 0
+            for i in range(len(tx_bodies) if tx_bodies else 0):
+                body = tx_bodies[i]
+                result = txs_results[i]
+                tx = amo.Tx(block.chain_id, block.height, i)
+                tx.parse_body(body)
+                tx.set_result(result)
+                if result['code'] == 0:
+                    num_valid += 1
+                else:
+                    num_invalid += 1
+                num += 1
+                tx.save(cur)
+
+            if num_valid > 0 or num_invalid > 0:
+                block.num_txs = num
+                block.num_txs_valid = num_valid
+                block.num_txs_invalid = num_invalid
+                block.update(cur)
+
+            ### NOTE: the following strategy is effective when tx density is low.
+
+            # XXX: This limit is due to the limit of tendermint rpc.
+            #batch_run = min(run,20)
+            # collect raw data
+            #metas = collect_block_headers(node, batch_base, batch_run)
+            #for meta in metas:
+            #    block = amo.format_block(meta)
+            #    save_block(block)
+            #    if int(block['num_txs']) > 0:
+            #        items = collect_block_txs(node, block['height'])
+            #        for item in items:
+            #            tx = amo.format_tx(item)
+            #            save_tx(block['chain_id'], tx)
+            # update stat
+            # done
+
+            #run -= len(metas)
+            #batch_base += len(metas)
+
+            self.db.commit()
+            # batch end
+        
+        self.height += run
+        print()
+        # closing
+        cur.close()
+
+def collect_block(s, node, height):
     r = s.get(f'{node}/block?height={height}')
     dat = json.loads(r.text)['result']
     block_id = dat['block_id']
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,12 +4,10 @@
 
 import json
 import argparse
-import requests
 import mysql.connector
 from mysql.connector import Error
 from datetime import timezone
 
-import amo
 import collector
 import builder
 
@@ -55,126 +53,10 @@ else:
     print("DB connected")
 
 if args.node:
-    cur = db.cursor()
-    s = requests.Session()
-
-    # get node status
-    r = s.get(f'{node}/status')
-    dat = json.loads(r.text)
-    target_height = int(dat['result']['sync_info']['latest_block_height'])
-    #print(dat['result']['node_info']['network'])
-    print(f'target remote height: {target_height}')
-    chain_id = dat['result']['node_info']['network']
-
-    # get current explorer state
-    cur.execute("""SELECT `height` FROM `c_blocks` 
-        WHERE (`chain_id` = %(chain_id)s)
-        ORDER BY `height` DESC LIMIT 1""",
-        {'chain_id': chain_id})
-    row = cur.fetchone()
-    if row:
-        #print(row)
-        b = dict(zip(cur.column_names, row))
-        last_height = int(b['height'])
-    else:
-        last_height = 0
-    print(f'currnet explorer height: {last_height}')
-
-    # check genesis
-    cur.execute("""
-        SELECT COUNT(*) FROM `c_genesis` WHERE (`chain_id` = %(chain_id)s)
-        """,
-        {'chain_id': chain_id})
-    row = cur.fetchone()
-    if row[0] == 0:
-        r = s.get(f'{node}/genesis')
-        dat = json.loads(r.text)
-        genesis = json.dumps(dat['result']['genesis'])
-        cur.execute("""
-            INSERT INTO `c_genesis`
-                (`chain_id`, `genesis`)
-            VALUES
-                (%(chain_id)s, %(genesis)s)
-            """,
-            {'chain_id': chain_id, 'genesis': genesis})
-        db.commit()
-
-    # figure out
-    limit = args.limit
-    if limit > 0:
-        run = min(target_height - last_height, limit)
-    else:
-        run = target_height - last_height
-
-    print(f'==========================================================')
-    batch_base = last_height
-    #while run > 0:
-    for h in range(last_height + 1, last_height + run + 1):
-        # batch start
-        db.autocommit = False
-
-        ### NOTE: the following strategy is effective when tx density is high.
-        if h % 50 == 0:
-            print('.', flush=True)
-        else:
-            print('.', end='', flush=True)
-        if h % 100 == 0:
-            print(f'block height {h}', flush=True)
-        block_id, block_raw, txs_results, incs = collector.block(s, node, h)
-        block_header = block_raw['header']
-        tx_bodies = block_raw['data']['txs']
-        block = amo.Block(block_header['chain_id'], block_header['height'])
-        block.set_meta(block_id, block_header)
-        block.incentives = incs
-        block.save(cur)
-        num = 0
-        num_valid = 0
-        num_invalid = 0
-        for i in range(len(tx_bodies) if tx_bodies else 0):
-            body = tx_bodies[i]
-            result = txs_results[i]
-            tx = amo.Tx(block.chain_id, block.height, i)
-            tx.parse_body(body)
-            tx.set_result(result)
-            if result['code'] == 0:
-                num_valid += 1
-            else:
-                num_invalid += 1
-            num += 1
-            tx.save(cur)
-
-        if num_valid > 0 or num_invalid > 0:
-            block.num_txs = num
-            block.num_txs_valid = num_valid
-            block.num_txs_invalid = num_invalid
-            block.update(cur)
-
-        ### NOTE: the following strategy is effective when tx density is low.
-
-        # XXX: This limit is due to the limit of tendermint rpc.
-        #batch_run = min(run,20)
-        # collect raw data
-        #metas = collect_block_headers(node, batch_base, batch_run)
-        #for meta in metas:
-        #    block = amo.format_block(meta)
-        #    save_block(block)
-        #    if int(block['num_txs']) > 0:
-        #        items = collect_block_txs(node, block['height'])
-        #        for item in items:
-        #            tx = amo.format_tx(item)
-        #            save_tx(block['chain_id'], tx)
-        # update stat
-        # done
-
-        #run -= len(metas)
-        #batch_base += len(metas)
-
-        db.commit()
-        # batch end
-
-    print()
-    # closing
-    cur.close()
+    collector = collector.Collector(node, db)
+    collector.stat()
+    collector.play(args.limit)
+    collector.stat()
 
 if args.chain:
     cur = db.cursor()
