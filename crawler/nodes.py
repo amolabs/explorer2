c018a114d4e4142650d0f5b7a02bbea5f38ab8bb
Yeon-Hyeong Yang <yh.yang@dcgraph.com>
2020-04-27 12:29:30 +0900

    [crawler] apply linting result

--- crawler/nodes.py
+++ crawler/nodes.py
@@ -14,12 +14,13 @@ p.add_argument('node', type=str, nargs='+')
 
 args = p.parse_args()
 
+
 def neighbors(addr):
     try:
         #print(f'collecting neighbors from {addr}')
         print('.', end='', flush=True)
         res = r.get(url=f'http://{addr}/net_info', timeout=REQUEST_TIMEOUT)
-    except:
+    except Exception:
         return []
     peers = json.loads(res.text)['result']['peers']
     ps = []
@@ -31,26 +32,30 @@ def neighbors(addr):
         ps.append(f'{ip}:{port}')
     return ps
 
+
 def peek(addr):
     try:
         #print(f'collecting information from {addr}')
         print('.', end='', flush=True)
         res = r.get(url=f'http://{addr}/status', timeout=REQUEST_TIMEOUT)
-    except:
+    except Exception:
         return {}
     node = json.loads(res.text)['result']
     return node
 
+
 def expand(node):
     node['moniker'] = node['node_info']['moniker']
     tcp_addr = node['node_info']['listen_addr'].split('tcp://')[1]
-    node['p2p_addr'] = node['node_info']['id']+'@'+tcp_addr
+    node['p2p_addr'] = node['node_info']['id'] + '@' + tcp_addr
     ip_addr = tcp_addr.split(':')[0]
-    rpc_port = node['node_info']['other']['rpc_address'].split('tcp://')[1].split(':')[1]
+    rpc_port = node['node_info']['other']['rpc_address'].split(
+        'tcp://')[1].split(':')[1]
     node['rpc_addr'] = f'http://{ip_addr}:{rpc_port}'
     node['catching_up_sign'] = '+' if node['sync_info']['catching_up'] else ' '
     node['voting_power'] = node['validator_info']['voting_power']
     return node
+
 
 cands = []
 for n in args.node:
@@ -67,9 +72,9 @@ while cands:
     n = cands.pop()
     if n not in nodes:
         peek_n = peek(n)
-        if peek_n == {}: # when cannot reach to node behind firewall via rpc
+        if peek_n == {}:  # when cannot reach to node behind firewall via rpc
             continue
-        nodes[n] = peek_n 
+        nodes[n] = peek_n
     peers = neighbors(n)
     for n in peers:
         if n not in nodes and n not in cands:
@@ -88,5 +93,6 @@ for _, n in nodes.items():
 print()
 for k in sorted(monikers.keys()):
     n = monikers[k]
-    print(f'{k:{mlen}} {n["rpc_addr"]:{alen}} {n["sync_info"]["latest_block_height"]:>7} {n["catching_up_sign"]} {n["voting_power"]:>{20}}')
-
+    print(
+        f'{k:{mlen}} {n["rpc_addr"]:{alen}} {n["sync_info"]["latest_block_height"]:>7} {n["catching_up_sign"]} {n["voting_power"]:>{20}}'
+    )
