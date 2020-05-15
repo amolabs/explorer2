5462eff981d4255227eb4430c7d4699d2cdbc40b
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-05-15 10:04:31 +0900

    Fix getHistory to handle anchor 0 properly

--- server/models/node.js
+++ server/models/node.js
@@ -68,7 +68,10 @@ async function getHistory(chain_id, node_id, anchor, from, num) {
     return new Promise(function(resolve, resect) {
         from = Number(from);
         num = Number(num);
-        var query_str = "SELECT \
+        var query_str;
+        var query_var;
+        if (anchor == 0) {
+          query_str = "SELECT \
             n.`node_id`, n.`moniker`, \
             INET_NTOA(n.`ip_addr`) `ip_addr`, i.`n_peers`, i.`val_addr`, \
             i.`latest_block_height`, i.`latest_block_time`, i.`catching_up` \
@@ -77,10 +80,24 @@ async function getHistory(chain_id, node_id, anchor, from, num) {
             JOIN `node_info` i ON n.`chain_id` = i.`chain_id` \
             AND n.`node_id` = i.`node_id` \
         WHERE n.`chain_id` = ? AND n.`node_id` = ? \
-        AND i.`latest_block_height` <= ? \
         ORDER BY i.`latest_block_height` DESC \
         LIMIT ?, ?";
-        var query_var = [chain_id, node_id, anchor, from, num];
+        query_var = [chain_id, node_id, from, num];
+        } else { 
+          query_str = "SELECT \
+              n.`node_id`, n.`moniker`, \
+              INET_NTOA(n.`ip_addr`) `ip_addr`, i.`n_peers`, i.`val_addr`, \
+              i.`latest_block_height`, i.`latest_block_time`, i.`catching_up` \
+          FROM \
+              `nodes` n \
+              JOIN `node_info` i ON n.`chain_id` = i.`chain_id` \
+              AND n.`node_id` = i.`node_id` \
+          WHERE n.`chain_id` = ? AND n.`node_id` = ? \
+          AND i.`latest_block_height` <= ? \
+          ORDER BY i.`latest_block_height` DESC \
+          LIMIT ?, ?";
+          query_var = [chain_id, node_id, anchor, from, num];
+        }
         db.query(query_str, query_var, function(err, rows, fields) {
             if (err) {
                 return reject(err);
