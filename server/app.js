4925a7ffbe6f376401598cf65881d648eaa345f2
Yeon-Hyeong Yang <lbird94@gmail.com>
2020-05-15 12:39:16 +0900

    [server] clean up

--- server/app.js
+++ server/app.js
@@ -1,18 +1,15 @@
 /* vim: set sw=2 ts=2 expandtab : */
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
 var app = express();
 
 app.use(logger('short'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(cors({origin: '*', optionsSuccessStatus: 200}));
 
 app.use('/', indexRouter);
