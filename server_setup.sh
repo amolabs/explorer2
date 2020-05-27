fdd5f015fcccd77c819b9a137cbdcb2aa53330f8
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-05-27 11:01:04 +0900

    Add server setup script

---
+++ server_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SERVICE_NAME=amo-api-server.service
+
+# copy server/
+rm -rf /root/amo-api-server
+cp -f server/ /root/amo-api-server
+
+# prepare service
+cp -f server.service.in /etc/systemd/system/$SERVICE_NAME
+
+# enable service
+systemctl enable $SERVICE_NAME
