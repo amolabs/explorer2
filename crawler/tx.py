b8300ac8d88f3fbf716fe2424a75940461129165
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-07-15 14:49:50 +0900

    Truncate wrong proxy_account

--- crawler/tx.py
+++ crawler/tx.py
@@ -236,7 +236,7 @@ def tx_register(tx, cursor):
     host = models.Account(tx.chain_id, storage.owner, cursor)
 
     parcel.custody = payload['custody']
-    parcel.proxy_account = payload.get('proxy_account', None)
+    parcel.proxy_account = payload.get('proxy_account', None)[:40]
     parcel.extra = payload.get('extra', '{}')
     parcel.on_sale = True
     parcel.save(cursor)
