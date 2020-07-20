cf54683cd0b6cec59331942c92a87723992440eb
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-07-20 19:24:32 +0900

    [api] Display uptime based on time range
    - TODO: make some note about default timezone(UTC) somewhere

--- server/models/node.js
+++ server/models/node.js
@@ -1,62 +1,85 @@
 /* vim: set sw=2 ts=2 expandtab : */
 const db = require('../db/db');
 
-async function getOne(chain_id, node_id) {
+async function getOne(chain_id, val_addr, from, to) {
   return new Promise(function(resolve, reject) {
     var query_str = "SELECT \
-        n.`node_id`, n.`moniker`, \
-        INET_NTOA(n.`ip_addr`) `ip_addr`, i.`n_peers`, i.`val_addr`, \
-        i.`latest_block_height`, i.`latest_block_time`, \
-        i.`catching_up`, i.`elapsed` \
-      FROM \
-        `nodes` n, `node_info` i \
-        JOIN ( \
-          SELECT \
-            `chain_id`, `node_id`, \
-            MAX(`latest_block_height`) `latest_block_height` \
-          FROM `node_info` \
-          WHERE `chain_id` = ? AND `node_id` = ? \
-          GROUP BY `node_id` \
-        ) m ON i.`chain_id` = m.`chain_id` \
-        AND i.`node_id` = m.`node_id` \
-        AND i.`latest_block_height` = m.`latest_block_height` \
-      WHERE n.`chain_id` = m.`chain_id` AND n.`node_id` = m.`node_id`";
-    query_var = [chain_id, node_id];
-    val = {};
-    db.query(query_str, query_var, function(err, rows, fields) {
+    n.`node_id`, n.`moniker`, ni.`val_addr`, \
+    ni.`catching_up`, ni.`elapsed`, ni.`timestamp`, \
+    ni.`latest_block_height`, ni.`latest_block_time`, \
+    ut.`uptime` \
+  FROM `nodes` n \
+  JOIN `node_info` ni \
+    ON n.`chain_id` = ni.`chain_id` \
+    AND n.`node_id` = ni.`node_id` \
+  JOIN ( \
+    SELECT o.`node_id`, COUNT(o.`timestamp`)/g.`count_t`*100 `uptime` \
+    FROM ( \
+      SELECT `node_id`, `timestamp` \
+      FROM `node_info` \
+      WHERE `chain_id` = ? AND `val_addr` = ? AND `timestamp` BETWEEN ? AND ? \
+    ) o \
+    JOIN ( \
+      SELECT COUNT(`timestamp`) `count_t` \
+      FROM `node_info` \
+      WHERE `chain_id` = 'ghost_chain_id' AND `timestamp` BETWEEN ? AND ? \
+    ) g \
+    GROUP BY o.`node_id` \
+  ) ut \
+  ON n.`node_id` = ut.`node_id` \
+  JOIN ( \
+    SELECT `node_id`, MAX(`timestamp`) `max_timestamp` \
+    FROM `node_info` \
+    WHERE `chain_id` = ? AND `val_addr` = ? AND `timestamp` BETWEEN ? AND ? \
+    GROUP BY `node_id` \
+  ) mt \
+  ON n.`node_id` = mt.`node_id` AND ni.`timestamp` = mt.`max_timestamp` \
+  ORDER BY n.`moniker`";
+    query_var = [chain_id, val_addr, from, to, from, to, chain_id, val_addr, from, to];
+    db.query(query_str, query_var, function(err, row, fields) {
         if(err) {
             return reject(err);
         }
-        return resolve(rows[0]);
+        return resolve(row);
     });
   });
 }
 
-async function getList(chain_id, from, num) {
+async function getList(chain_id, from, to) {
     return new Promise(function(resolve, reject) {
-        from = Number(from);
-        num = Number(num);
         var query_str = "SELECT \
-            n.`node_id`, n.`moniker`, \
-            INET_NTOA(n.`ip_addr`) `ip_addr`, i.`n_peers`, i.`val_addr`, \
-            i.`latest_block_height`, i.`latest_block_time`, \
-            i.`catching_up`, i.`elapsed` \
-          FROM \
-            `nodes` n, `node_info` i \
+              n.`node_id`, n.`moniker`, ni.`val_addr`, \
+              ni.`catching_up`, ni.`elapsed`, ni.`timestamp`, \
+              ni.`latest_block_height`, ni.`latest_block_time`, \
+              ut.`uptime` \
+            FROM `nodes` n \
+            JOIN `node_info` ni \
+              ON n.`chain_id` = ni.`chain_id` \
+              AND n.`node_id` = ni.`node_id` \
             JOIN ( \
-              SELECT \
-                `chain_id`, `node_id`, \
-                MAX(`latest_block_height`) `latest_block_height` \
+              SELECT o.`node_id`, COUNT(o.`timestamp`)/g.`count_t`*100 `uptime` \
+              FROM ( \
+                SELECT `node_id`, `timestamp` \
+                FROM `node_info` \
+                WHERE `chain_id` = ? AND `timestamp` BETWEEN ? AND ? \
+              ) o \
+              JOIN ( \
+                SELECT COUNT(`timestamp`) `count_t` \
+                FROM `node_info` \
+                WHERE `chain_id` = 'ghost_chain_id' AND `timestamp` BETWEEN ? AND ? \
+              ) g \
+              GROUP BY o.`node_id` \
+            ) ut \
+            ON n.`node_id` = ut.`node_id` \
+            JOIN ( \
+              SELECT `node_id`, MAX(`timestamp`) `max_timestamp` \
               FROM `node_info` \
-              WHERE `chain_id` = ? \
+              WHERE `chain_id` = ? AND `timestamp` BETWEEN ? AND ? \
               GROUP BY `node_id` \
-            ) m ON i.`chain_id` = m.`chain_id` \
-            AND i.`node_id` = m.`node_id` \
-            AND i.`latest_block_height` = m.`latest_block_height` \
-          WHERE n.`chain_id` = m.`chain_id` AND n.`node_id` = m.`node_id` \
-          ORDER BY n.`moniker` \
-          LIMIT ?, ?";
-        var query_var = [chain_id, from, num];
+            ) mt \
+            ON n.`node_id` = mt.`node_id` AND ni.`timestamp` = mt.`max_timestamp` \
+            ORDER BY n.`moniker`";
+        var query_var = [chain_id, from, to, from, to, chain_id, from, to];
         db.query(query_str, query_var, function(err, rows, fields) {
             if (err) {
                 return reject(err);
@@ -66,40 +89,37 @@ async function getList(chain_id, from, num) {
     });
 }
 
-async function getHistory(chain_id, node_id, anchor, from, num) {
+async function getHistory(chain_id, val_addr, anchor, from, num) {
     return new Promise(function(resolve, resect) {
-        from = Number(from);
-        num = Number(num);
         var query_str;
         var query_var;
         if (anchor == 0) {
           query_str = "SELECT \
-            n.`node_id`, n.`moniker`, \
-            INET_NTOA(n.`ip_addr`) `ip_addr`, i.`n_peers`, i.`val_addr`, \
+            n.`node_id`, n.`moniker`, i.`val_addr`, \
             i.`latest_block_height`, i.`latest_block_time`, \
-            i.`catching_up`, i.`elapsed` \
+            i.`catching_up`, i.`elapsed`, i.`timestamp` \
         FROM \
             `nodes` n \
             JOIN `node_info` i ON n.`chain_id` = i.`chain_id` \
             AND n.`node_id` = i.`node_id` \
-        WHERE n.`chain_id` = ? AND n.`node_id` = ? \
+        WHERE n.`chain_id` = ? AND i.`val_addr` = ? \
         ORDER BY i.`latest_block_height` DESC \
         LIMIT ?, ?";
-        query_var = [chain_id, node_id, from, num];
+        query_var = [chain_id, val_addr, from, num];
         } else { 
           query_str = "SELECT \
-              n.`node_id`, n.`moniker`, \
-              INET_NTOA(n.`ip_addr`) `ip_addr`, i.`n_peers`, i.`val_addr`, \
-              i.`latest_block_height`, i.`latest_block_time`, i.`catching_up` \
+              n.`node_id`, n.`moniker`, i.`val_addr`, \
+              i.`latest_block_height`, i.`latest_block_time`, \
+              i.`catching_up`, i.`elapsed`, i.`timestamp` \
           FROM \
               `nodes` n \
               JOIN `node_info` i ON n.`chain_id` = i.`chain_id` \
               AND n.`node_id` = i.`node_id` \
-          WHERE n.`chain_id` = ? AND n.`node_id` = ? \
+          WHERE n.`chain_id` = ? AND i.`val_addr` = ? \
           AND i.`latest_block_height` <= ? \
           ORDER BY i.`latest_block_height` DESC \
           LIMIT ?, ?";
-          query_var = [chain_id, node_id, anchor, from, num];
+          query_var = [chain_id, val_addr, anchor, from, num];
         }
         db.query(query_str, query_var, function(err, rows, fields) {
             if (err) {
--- server/routes/nodes.js
+++ server/routes/nodes.js
@@ -5,9 +5,27 @@ const node = require('../models/node');
 
 router.get('/', function(req, res) {
   const chain_id = res.locals.chain_id;
-  var from = req.query.from || 0;
-  var num = req.query.num || 20;
-  node.getList(chain_id, from, num)
+  var range = req.query.range || 60; // seconds
+
+  var to = new Date();
+  var to_timestamp = to.getUTCFullYear() + '-' 
+                   + (to.getUTCMonth()+1) + '-'
+                   + to.getUTCDate() + ' '
+                   + to.getUTCHours() +':'
+                   + to.getUTCMinutes() + ':'
+                   + to.getUTCSeconds();
+  to_timestamp = req.query.upto || to_timestamp;
+  var from = new Date(Date.parse(to_timestamp) - (range * 1000));
+  var from_timestamp = from.getUTCFullYear() + '-' 
+                     + (from.getUTCMonth()+1) + '-'
+                     + from.getUTCDate() + ' '
+                     + from.getUTCHours() +':'
+                     + from.getUTCMinutes() + ':'
+                     + from.getUTCSeconds();
+
+  console.log(from_timestamp, "~", to_timestamp);
+
+  node.getList(chain_id, from_timestamp, to_timestamp)
     .then((row) => {
       res.status(200);
       res.send(row);
@@ -18,9 +36,26 @@ router.get('/', function(req, res) {
     });
 });
 
-router.get('/:node_id([a-fA-F0-9]+)', function(req, res) {
+router.get('/:val_addr([a-fA-F0-9]+)', function(req, res) {
   const chain_id = res.locals.chain_id;
-  node.getOne(chain_id, req.params.node_id)
+  const val_addr = req.params.val_addr;
+  var range = req.query.range || 60; // seconds
+  var to = new Date();
+  var to_timestamp = to.getUTCFullYear() + '-' 
+                   + (to.getUTCMonth()+1) + '-'
+                   + to.getUTCDate() + ' '
+                   + to.getUTCHours() +':'
+                   + to.getUTCMinutes() + ':'
+                   + to.getUTCSeconds();
+  to_timestamp = req.query.upto || to_timestamp;
+  var from = new Date(Date.parse(to_timestamp) - (range * 1000));
+  var from_timestamp = from.getUTCFullYear() + '-' 
+                     + (from.getUTCMonth()+1) + '-'
+                     + from.getUTCDate() + ' '
+                     + from.getUTCHours() +':'
+                     + from.getUTCMinutes() + ':'
+                     + from.getUTCSeconds();
+  node.getOne(chain_id, val_addr, from_timestamp, to_timestamp)
     .then((row) => {
       if (row) {
         res.status(200);
@@ -36,12 +71,13 @@ router.get('/:node_id([a-fA-F0-9]+)', function(req, res) {
     });
 });
 
-router.get('/:node_id([a-fA-F0-9]+)/history', function(req, res) {
+router.get('/:val_addr([a-fA-F0-9]+)/history', function(req, res) {
   const chain_id = res.locals.chain_id;
+  const val_addr = req.params.val_addr;
   var anchor = req.query.anchor || 0;
   var from = req.query.from || 0;
   var num = req.query.num || 20;
-  node.getHistory(chain_id, req.params.node_id, anchor, from, num)
+  node.getHistory(chain_id, val_addr, anchor, from, num)
     .then((row) => {
       if (row) {
         res.status(200);
