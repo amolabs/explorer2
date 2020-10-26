da1ba2ab66e49692a4a076f190492ed2a5690e6d
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-10-26 10:25:58 +0900

    Dockerize crawler-builder individually

---
+++ server/crawler/builder/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$CHAIN_ID" ]; then
+    echo "Environment variable 'CHAIN_ID' is not given"
+    exit 1
+fi
+
+echo "chain id         = $CHAIN_ID"
+
+./builder.py --chain $CHAIN_ID --limit 0
