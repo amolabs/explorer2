4bce48dd9cc08853f7c2823fad7d4a89ca2a7e79
Jaehong Ahn <monsieur.ahn@gmail.com>
2020-10-26 13:15:54 +0900

    Dockerize crawler-nodes individually

---
+++ server/crawler/nodes/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z "$TARGET_ADDRS" -o -z "$CHAIN_ID" ]; then
+    echo "Environment variable 'TARGET_ADDRS' or 'CHAIN_ID' is not given"
+    exit 1
+fi
+
+if [ -z "$REFRESH_INTERVAL" ]; then
+    REFRESH_INTERVAL=1800
+fi
+
+if [ -z "$COLLECT_INTERVAL" ]; then
+    COLLECT_INTERVAL=10
+fi
+
+echo "target addrs     = $TARGET_ADDRS"
+echo "chain id         = $CHAIN_ID"
+echo "refresh interval = $REFRESH_INTERVAL"
+echo "collect interval = $COLLECT_INTERVAL"
+
+./nodes.py --chain $CHAIN_ID --targets $TARGET_ADDRS -rit $REFRESH_INTERVAL -cit $COLLECT_INTERVAL
