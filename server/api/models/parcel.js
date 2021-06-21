be22d30f42d9e8696f46768db1355b55c2867156
Yeon-Hyeong Yang <lbird94@gmail.com>
2021-06-21 15:02:31 +0900

    [api] get parcel list by owner

--- server/api/models/parcel.js
+++ server/api/models/parcel.js
@@ -20,9 +20,62 @@ async function getOne(chain_id, parcel_id) {
   });
 }
 
+async function getListByOwner(chain_id, address, top, from, num) {
+  return new Promise(function(resolve, reject) {
+    top = Number(top);
+    from = Number(from);
+    num = Number(num);
+    var query_str;
+    var query_var;
+    if (top == 0) {
+      query_str = "SELECT * \
+        FROM s_parcels sp LEFT JOIN ( \
+          SELECT rpt.parcel_id, MAX(rpt.height) h, ct.`type` \
+            FROM r_parcel_tx rpt \
+          LEFT JOIN c_txs ct \
+            ON rpt.chain_id = ct.chain_id AND rpt.height = ct.height \
+              AND rpt.`index` = ct.`index` \
+          GROUP BY rpt.parcel_id \
+        ) t ON sp.parcel_id = t.parcel_id \
+        WHERE sp.chain_id = ? \
+          AND sp.owner = ? \
+          AND t.`type` = 'register' \
+        ORDER BY t.h DESC \
+        LIMIT ?, ? \
+      ";
+      query_var = [chain_id, address, from, num];
+    } else {
+      query_str = "SELECT * \
+        FROM s_parcels sp LEFT JOIN ( \
+          SELECT rpt.parcel_id, MAX(rpt.height) h, ct.`type` \
+            FROM r_parcel_tx rpt \
+          LEFT JOIN c_txs ct \
+            ON rpt.chain_id = ct.chain_id AND rpt.height = ct.height \
+              AND rpt.`index` = ct.`index` \
+          GROUP BY rpt.parcel_id \
+        ) t ON sp.parcel_id = t.parcel_id \
+        WHERE sp.chain_id = ? \
+          AND sp.owner = ? \
+          AND t.`type` = 'register' \
+          AND h <= ? \
+        ORDER BY t.h DESC \
+        LIMIT ?, ? \
+      ";
+      query_var = [chain_id, address, top, from, num];
+    }
+    db.query(query_str, query_var, function (err, rows, fields) {
+      if (err) {
+        return reject(err);
+      }
+      resolve(rows);
+    });
+  });
+}
+
 module.exports = {
   //getStat: getStat,
   getOne: getOne,
   //getLast: getLast,
   //getList: getList,
+  getListByOwner: getListByOwner,
 }
--- server/api/routes/accounts.js
+++ server/api/routes/accounts.js
@@ -3,6 +3,7 @@ var express = require('express');
 var router = express.Router();
 const account = require('../models/account');
 const tx = require('../models/tx');
+const parcel = require('../models/parcel');
 const relation = require('../models/relation');
 const handleAccountIncentives = require('./incentives').handleAccountIncentives;
 const handleAccountPenalties = require('./penalties').handleAccountPenalties;
@@ -326,6 +327,62 @@ router.get('/:address([a-fA-F0-9]+)/history', function(req, res) {
   const address = req.params.address;
   relation.getAccountHistory(chain_id, address, anchor, from, num,
     include_tx, include_block)
+    .then((rows) => {
+      res.status(200);
+      res.send(rows);
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send(err);
+    });
+});
+
+/**
+ * @swagger
+ * /chain/{chain_id}/accounts/{address}/parcels:
+ *   parameters:
+ *     - $ref: '#/definitions/ChainId'
+ *     - $ref: '#/definitions/Address'
+ *     - name: top
+ *       in: query
+ *       description: anchor height to query (0 value means last block)
+ *       schema:
+ *         type: integer
+ *         default: 0
+ *     - name: from
+ *       in: query
+ *       description: offset from the result
+ *       schema:
+ *         type: integer
+ *         default: 0
+ *     - name: num
+ *       in: query
+ *       description: number of items to retrieve
+ *       schema:
+ *         type: integer
+ *         default: 20
+ *   get:
+ *     tags:
+ *       - accounts
+ *     description: Get parcel list by owner address
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Parcel list
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/definitions/ParcelInfo'
+ */
+router.get('/:address([a-fA-F0-9]+)/parcels', function(req, res) {
+  const chain_id = res.locals.chain_id;
+  var top = req.query.top || 0;
+  var from = req.query.from || 0;
+  var num = req.query.num || 20;
+  parcel.getListByOwner(chain_id, req.params.address, top, from, num)
     .then((rows) => {
       res.status(200);
       res.send(rows);
